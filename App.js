c0097cab2a776e1d0b725c888b0372c54c494581
AnaGiih <127118777+AnaGiih@users.noreply.github.com>
2023-08-14 12:17:56 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 const App = () =>{
   const [inputValues, setInputValues]= useState({
 input1: '',
+ input2: '',
+
 
   });
 
@@ -14,39 +16,47 @@ input1: '',
   };
 
   const handleSum = () =>{
-    const {input1} = inputValues;
-    const result = parseInt(input1);
+    const {input1, input2} = inputValues;
+    const result = parseInt(input1) + parseInt(input2);
+    
 
     setSum(result);
   };
-
+ 
   return(
     <View style={style.container}>
     <Text style={style.label}>input1:</Text>
     <TextInput
     style={style.input}
-    onChangeText={(value) => handleInputChange('input1', value)}
+    onChangeText={(value) => handleInputChange('input1',value)}
     value = {inputValues.input1}
     keyboardType="numeric"
     />
+    <Text style={style.label}>input2:</Text>
+    <TextInput
+    style={style.input}
+    onChangeText={(value) => handleInputChange('input2',value)}
+    value = {inputValues.input2}
+    keyboardType="numeric"
+    />
+    
 
     <Button title="Somar" onPress={handleSum}/>
     <Text style={style.result}>Resultado: {sum} </Text>
 
     </View>
   )
-
 };
 
 const style= StyleSheet.create ({
 container:{
   flex: 1,
-  backgroundColor: "#136798",
+  backgroundColor: "#3699",
   padding: 20,
+  
 },
 
 
 });
 
 export default App;
-
